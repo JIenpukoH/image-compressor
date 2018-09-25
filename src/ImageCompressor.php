fd2f89d475c13154bd7a2a80bb602dd12e8e216b
Asset <mamiraimov.aset@gmail.com>
2018-09-25 11:51:15 +0600

    get output name

--- src/ImageCompressor.php
+++ src/ImageCompressor.php
@@ -9,7 +9,6 @@
 namespace JIenpukoH\ImageCompressor\Compressor;
 
 
-
 use JIenpukoH\ImageCompressor\Command;
 
 class ImageCompressor
@@ -23,58 +22,59 @@ class ImageCompressor
      * @param bool $move
      * @throws \Exception
      */
-    public function compress($filename,$quality = 85,$move = true)
+    public function compress($filename, $quality = 85, $move = true)
     {
         $path = pathinfo($filename);
-        if(!isset($path['extension'])){
+        if (!isset($path['extension'])) {
             throw new \Exception('File Extension not found');
         }
         $extension = $path['extension'];
         $dirname = $path['dirname'];
         $name = $path['filename'];
 
-        if(mb_strtolower($extension, 'UTF-8') == 'jpg'){
-            if($move){
-                $dest_dir = $dirname.'/'.$this->optimized_dir;
+        if (mb_strtolower($extension, 'UTF-8') == 'jpg') {
+            if ($move) {
+                $dest_dir = $dirname . '/' . $this->optimized_dir;
                 if (!is_dir($dest_dir)) {
                     mkdir($dest_dir);
                 }
-            } else{
+            } else {
                 $dest_dir = $dirname;
             }
-            $output_file = $this->getOutputName($quality, $dest_dir, $name, $extension);
+            $output_file = $this->getOutputName($dest_dir, $name, $quality, $extension);
 
             $options = [
-                ''.$filename,
+                '' . $filename,
                 '-sampling-factor 4:2:0',
                 '-strip',
-                '-quality '.$quality,
+                '-quality ' . $quality,
                 '-interlace JPEG',
-                ''.$output_file
+                '' . $output_file
             ];
 
-            try{
-                $command = new Command('convert',$options);
+            try {
+                $command = new Command('convert', $options);
                 $command->execute();
-            } catch (\Exception $e){
+            } catch (\Exception $e) {
 
             }
         }
     }
 
-    public function optimizedExist($filename,$quality = 85)
+    public function optimizedExist($filename, $quality = 85)
     {
         $path = pathinfo($filename);
-        $dest_dir = $path['dirname'].'/'.$this->optimized_dir;
-        return file_exists($dest_dir.'/'.$path['filename'].'-q-'.$quality.'.'.$path['extension']);
+        $dest_dir = $path['dirname'] . '/' . $this->optimized_dir;
+        return file_exists($this->getOutputName($dest_dir, $path['filename'], $quality, $path['extension']));
     }
 
-    public function getOptimized($dirname,$filename,$quality = 85)
+    public function getOptimized($dirname, $filename, $quality = 85)
     {
         $path = pathinfo($filename);
-        $dest_dir = $dirname.'/'.$this->getOptimizedDir().'/' ;
-        return $dest_dir.$path['filename'].'-q-'.$quality.'.'.$path['extension'];
+        $dest_dir = $dirname . '/' . $this->getOptimizedDir();
+        return $this->getOutputName($dest_dir, $path['filename'], $quality, $path['extension']);
     }
+
     public function getOptimizedDir()
     {
         return $this->optimized_dir;
@@ -87,7 +87,7 @@ class ImageCompressor
      * @param $extension
      * @return string
      */
-    public function getOutputName($quality, $dest_dir, $name, $extension)
+    public function getOutputName($dest_dir, $name, $quality, $extension)
     {
         return $dest_dir . '/' . $name . '-q-' . $quality . '.' . $extension;
     }
